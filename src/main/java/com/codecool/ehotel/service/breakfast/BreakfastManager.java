5ad6f7dcc2cd8866dc47e3c06b337849fe5549b7
Jon <ldegjon@gmail.com>
2023-05-24 12:11:51 +0200

    changed the time to now instead of using every timecycle

--- src/main/java/com/codecool/ehotel/service/breakfast/BreakfastManager.java
+++ src/main/java/com/codecool/ehotel/service/breakfast/BreakfastManager.java
@@ -20,9 +20,9 @@ public class BreakfastManager {
 
     public void serve(Set<Guest> guests){
         DisplayMetrics displayMetrics = new DisplayMetrics();
-        LocalTime cycleStart = LocalTime.of(6,0, 0 );
-        LocalTime cycleEnd = LocalTime.of(6,30, 0 );
-
+//        LocalTime cycleStart = LocalTime.of(6,0, 0 );
+  //      LocalTime cycleEnd = LocalTime.of(6,30, 0 );
+        LocalTime now = LocalTime.of(6,0,0);
         Map<MealType, Integer> meals = new HashMap<>();
 
         meals.put(MealType.BUN, 1);
@@ -33,29 +33,30 @@ public class BreakfastManager {
         int unhappyGuests = 0;
         int wasteCost= 0;
 
-        while (cycleEnd.isBefore(LocalTime.of(10,0,0))){
+        while (now.isBefore(LocalTime.of(10,0,0))){
 
             buffetService.refill(meals, buffet);
 
             for ( Guest guest : guests){
-                for(int i = buffet.meals().size() -1; i >= 0; i--){
-                    if(guest.guestType().getMealPreferences().contains(buffet.meals().get(i).mealType())){
-                        if(!buffetService.consumeFreshest(buffet.meals().get(i).mealType(), buffet)) {
+
+                for (Map.Entry<MealType, Integer> entry : meals.entrySet()){
+                    MealType mealType = entry.getKey();
+                    if(guest.guestType().getMealPreferences().contains(mealType)){
+                        if(!buffetService.consumeFreshest(mealType, buffet)){
                             unhappyGuests++;
-                        };
-                    };
+                        }
+                    }
                 }
             }
-            if(cycleEnd.isAfter(LocalTime.of(6, 0, 0).plusHours(1).plusMinutes(30))) {
-                wasteCost += buffetService.collectWaste(MealDurability.SHORT, cycleEnd.minusHours(1).minusMinutes(30), buffet);
-            }
+            wasteCost += buffetService.collectWaste(MealDurability.SHORT, now.minusHours(1).minusMinutes(30), buffet);
+            System.out.println(now);
+            System.out.println(wasteCost);
 
-            cycleStart = cycleStart.plusMinutes(30);
-            cycleEnd = cycleEnd.plusMinutes(30);
+            now = now.plusMinutes(30);
         }
 
-        wasteCost += buffetService.collectWaste(MealDurability.SHORT, cycleEnd, buffet);
-        wasteCost += buffetService.collectWaste(MealDurability.MEDIUM, cycleEnd, buffet);
+        wasteCost += buffetService.collectWaste(MealDurability.SHORT, now, buffet);
+        wasteCost += buffetService.collectWaste(MealDurability.MEDIUM, now, buffet);
 
         displayMetrics.displayMetrics(unhappyGuests, wasteCost);
     }

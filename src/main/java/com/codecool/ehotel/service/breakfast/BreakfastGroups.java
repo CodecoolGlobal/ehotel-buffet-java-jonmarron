277154fff504b87921fd47f8876160dc327f86af
Jon <ldegjon@gmail.com>
2023-05-26 09:04:38 +0200

    fixed some stuff

--- src/main/java/com/codecool/ehotel/service/breakfast/BreakfastGroups.java
+++ src/main/java/com/codecool/ehotel/service/breakfast/BreakfastGroups.java
@@ -9,12 +9,18 @@ import java.util.Set;
 
 public class BreakfastGroups {
     public Set<List<Guest>> prepareBreakfastGroups(Set<Guest> guests) {
+
         int setSize = guests.size();
         int guestPerSet = setSize / 7;
+        int remainingElements = setSize % 7;
+
         Set<List<Guest>> separatedSets = new HashSet<>();
-        for (int i = 0; i < 8 - 1; i++) {
+
+
+        for (int i = 0; i < 8-1; i++) {
             List<Guest> subSet = new ArrayList<>();
             for (int j = 0; j < guestPerSet; j++) {
+
                 Guest guest = guests.iterator().next();
                 subSet.add(guest);
                 guests.remove(guest);
